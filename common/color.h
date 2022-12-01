2f20af7ecab27cb6122c4f5573de23a7dfa5644d
Xiaojie-Huang <1961895405@qq.com>
2022-12-01 21:39:00 +0800

    first commit

---
+++ common/color.h
@@ -0,0 +1,17 @@
+#ifndef COLOR_H
+#define COLOR_H
+//pragma once和IF区别
+
+#include "vec3.h"
+
+#include <iostream>
+
+void write_color(std::ostream &out,color pixel_color)
+{
+    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+        << static_cast<int>(255.999 * pixel_color.y()) << ' '
+        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+}
+
+#endif
+
